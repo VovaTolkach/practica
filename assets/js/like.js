4783742cc78855607e2eafce55a4e82bb878ea45
Mykhailo <m.krutii.fd@gmail.com>
2023-07-20 00:31:35 +0300

    Change like-menu color, added backlight of like and change logic of like-system

--- assets/js/like.js
+++ assets/js/like.js
@@ -1,16 +1,37 @@
+// Отримуємо інформацію про лайкнуті товари з local storage
+var likedItems = JSON.parse(localStorage.getItem('likedItems')) || {};
+
 $(document).ready(function() {
+    // Проходимося по всім лайкнутим товарам і додаємо клас 'liked' для відповідних товарів
+    $('.like-icon').each(function() {
+        var itemId = $(this).closest('.trainer-item').data('item');
+        if (likedItems[itemId]) {
+            $(this).addClass('liked');
+        }
+    });
+
     // Код для зміни іконки лайка при натисканні
     $('.like-icon').click(function() {
         $(this).toggleClass('liked');
 
+        // Отримуємо data-item атрибут товару, який містить унікальний ідентифікатор
+        var itemId = $(this).closest('.trainer-item').data('item');
+
+        // Зберігаємо інформацію про лайк в local storage
+        likedItems[itemId] = $(this).hasClass('liked');
+        localStorage.setItem('likedItems', JSON.stringify(likedItems));
+
         // Оновлюємо блок liked-items-wrapper згідно стану лайків
         updateLikedItems();
     });
+
+    // Оновлюємо блок liked-items-wrapper при завантаженні сторінки
+    updateLikedItems();
 });
 
 function updateLikedItems() {
     var likedItems = $('.like-icon.liked').map(function() {
-        return $(this).parent().siblings('.down-content').find('h4').text();
+        return $(this).closest('.trainer-item').find('h4').text();
     }).get();
 
     var likedItemsList = $('#liked-items-list');
